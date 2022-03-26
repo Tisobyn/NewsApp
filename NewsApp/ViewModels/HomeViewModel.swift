6632e6e1a8e3984cc2f049aa048770177eb60393
Tiso <tisobyn@gmail.com>
2022-03-26 23:45:40 +0600

    fix: Some bugs

--- NewsApp/ViewModels/HomeViewModel.swift
+++ NewsApp/ViewModels/HomeViewModel.swift
@@ -17,37 +17,34 @@ class HomeViewModel: ObservableObject, HomeVMP {
     private let network: NetworkManager
     private(set) var articles = [Article]()
     private var cancellables = Set<AnyCancellable>()
+    
     @Published private(set) var state: ResultState = .loading
 
     init(network: NetworkManager) {
         self.network = network
+        getArticles()
     }
 
     func getArticles() {
         self.state = .loading
-//        let cancellable = network
-//            .request(from: NewsAPI.getArticles)
-//            .sink { result in
-//                switch result {
-//                case .finished:
-//                    self.state = .success(content: self.articles)
-//                case .failure(let error):
-//                    self.state = .failed(error: error)
-//                }
-//            } receiveValue: { response in
-//                guard let response = response as? NewsResponse else {
-//                    fatalError()
-//                }
-//                self.articles = response.articles
-//            }
-//
-//        self.cancellables.insert(cancellable)
+        let cancellable = network
+            .request(from: NewsAPI.getArticles, object: NewsResponse.self)
+            .sink { result in
+                switch result {
+                case .finished:
+                    self.state = .success(content: self.articles)
+                    print(self.articles)
+                case .failure(let error):
+                    self.state = .failed(error: error)
+                }
+            } receiveValue: { response in
+                self.articles = response.articles
+            }
+
+        self.cancellables.insert(cancellable)
     }
 
 
-  //    func setImage() {
-  //        //        thumbnailImageView.loadImageFromURL(urlString: photoURL)
-  //    }
 }
 
 
